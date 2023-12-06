ff1ce5a6a3a473dfbeacc83219d038217ea22b49
MOHANAPRIYA B <138850478+Mohanapriya64@users.noreply.github.com>
2023-12-05 19:25:02 -0800

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to check if it's a palindrome: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
