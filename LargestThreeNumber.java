bd8006089e0c80bb461936466d296d1bd190f0f7
MOHANAPRIYA B <138850478+Mohanapriya64@users.noreply.github.com>
2023-12-05 19:38:59 -0800

    Create LargestThreeNumber.java

---
+++ LargestThreeNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LargestOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double largestNumber = findLargest(num1, num2, num3);
+
+        System.out.println("The largest number is: " + largestNumber);
+
+        scanner.close();
+    }
+
+    public static double findLargest(double num1, double num2, double num3) {
+        if (num1 >= num2 && num1 >= num3) {
+            return num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
